569783c82cfc03c4fe973924bab0849a34815e8b
sarah <sarah@gmail>
2017-05-10 22:03:27 +0200

    sara hub test
    test

---
+++ MyAPI/MyAPI/Models/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAPI.Models
+{
+    public class Class2
+    {
+        public int id { get; set; }
+    }
+}
