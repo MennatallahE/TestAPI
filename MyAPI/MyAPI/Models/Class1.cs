b9294fd5d3bedf480480460b1bb2c9c0d6f5a878
Esraa <Esraa@DESKTOP-M6OLVLF>
2017-05-10 17:35:04 +0200

    firstCommit

---
+++ MyAPI/MyAPI/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAPI.Models
+{
+    public class Class1
+    {
+        public int sal { get; set; }
+    }
+}
